ad48add483146f55cdd5f68b5d4665d40cfd19c8
John Campusano <john024996@gmail.com>
2024-12-15 14:25:04 -0400

    ...

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -24,9 +24,22 @@ android {
         }
     }
     buildTypes {
-        getByName("release") {
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-DEBUG"
+            isDebuggable = true
             isMinifyEnabled = false
         }
+
+        getByName("release") {
+            isMinifyEnabled = true
+            isDebuggable = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
     }
     compileOptions {
         sourceCompatibility = BuildVersion.environment.javaVersion
